21b75c0070c08c246e1064f84e9b89ac5687129f
Mike Bender <mikebender@deephaven.io>
2023-05-24 12:14:44 -0400

    Add new permissions, and comment out the default values (#6)
    * Add new permissions, and comment out the default values
    
    - Added descriptions for each permission
    - Clearer now you only need to return permissions you wish to override
    
    * Fix typo

--- examples/ExampleLoginPlugin.jsx
+++ examples/ExampleLoginPlugin.jsx
@@ -1,6 +1,10 @@
 /* eslint-disable */
 import { Component } from "react";
 
+/**
+ * Plugin to override user permissions.
+ * Permissions are for UI functionality only.
+ */
 class ExampleLoginPlugin extends Component {
   constructor(props) {
     super(props);
@@ -10,13 +14,35 @@ class ExampleLoginPlugin extends Component {
     const { onPermissionOverrides } = this.props;
     onPermissionOverrides((username, operateAs, groups) => {
       console.log("[ExampleLoginPlugin]", username, operateAs, groups);
+
+      /** Only return the permissions you wish to override. Any permissions not provided will use their default value. */
       return {
-        canUsePanels: true,
-        canCreateDashboard: true,
-        canCreateCodeStudio: true,
-        canCreateQueryMonitor: true,
-        canCopy: true,
-        canDownloadCsv: true,
+        /** Is a super user */
+        // isSuperUser: false,
+        /** Only the summary view of queries is visible, and cannot create code studios */
+        // isQueryViewOnly: false,
+        /** Not allowed to use the web UI at all */
+        // isNonInteractive: false,
+        /** Can use the Panels menu to select panels to add to a dashboard */
+        // canUsePanels: true,
+        /** Can create new dashboards */
+        // canCreateDashboard: true,
+        /** Can create/use code studios */
+        // canCreateCodeStudio: true,
+        /** Can create/use query monitor */
+        // canCreateQueryMonitor: true,
+        /** Can copy table data using keyboard shortcuts or context menu */
+        // canCopy: true,
+        /** Can download/export table data as CSV */
+        // canDownloadCsv: true,
+        /** Can share dashboards with other users */
+        // canShareDashboards: true,
+        /** Can view the list of users a dashboard is shared with for a dashboard they are a viewer of */
+        // canViewDashboardSharedUsers: true,
+        /** Can share queries with other users */
+        // canShareQueries: true,
+        /** Can view the list of users a query is shared with for a query they are a viewer of */
+        // canViewQuerySharedUsers: true,
       };
     });
   }
